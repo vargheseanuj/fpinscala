ae34639cba24a4430286594fe33cb990979f7c6c
anuj varghese <vargheseanuj@gmail.com>
2022-02-04 10:37:19 +0530

    day 5

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,15 +7,32 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  def size[A](t: Tree[A], cnt:Int = 0):Int = t match {
-    case Leaf(_) => cnt +1
+  def size[A](t: Tree[A], cnt: Int = 0): Int = t match {
+    case Leaf(_) => cnt + 1
     case Branch(l, r) => size(l, cnt) + size(r, cnt)
   }
 
-  def depth[A](value: A, t: Tree[A]):Int = ???
+  def depth[A](value: A, t: Tree[A]): Int = {
+    def recurse(th: Tree[A], depth: Int): Int =
+      th match {
+        case Leaf(v) => if (v == value) depth else -1
+        case Branch(l, r) =>
+          val left = recurse(l, depth + 1)
+          val right = recurse(r, depth + 1)
+          if (left > -1) left
+          else if (right > -1) right
+          else -1
+      }
 
-  def maximum[A](t: Tree[A]):A = ???
+    recurse(t, 0)
+  }
 
-
-
+  def maximum(t: Tree[Int]): Int = {
+    def findMax(th: Tree[Int], max: Int): Int =
+      th match {
+        case Leaf(v) => max.max(v)
+        case Branch(l, r) => findMax(l, max).max(findMax(r, max))
+      }
+    findMax(t, Int.MinValue)
+  }
 }
