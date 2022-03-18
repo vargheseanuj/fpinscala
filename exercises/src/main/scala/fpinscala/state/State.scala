d063289a4cd9900779dfd781d45ff021e71062cf
anuj varghese <vargheseanuj@gmail.com>
2022-03-18 23:37:33 +0530

    day 10

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,6 +30,12 @@ object RNG {
       (f(a), rng2)
     }
 
+  def mapUsingFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2UsingFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, r) = rng.nextInt
     if (i == Int.MinValue) nonNegativeInt(rng) else (math.abs(i), r)
@@ -74,18 +80,29 @@ object RNG {
     (f(i, j),r2)
   }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1) // We pass the new state along
+    }
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
-    ???
+    flatMap(a => State(s => (f(a), s)))
+
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
+    flatMap(a => sb.flatMap(b => State(s => (f(a, b), s))))
+
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    State(s => {
+      val (a, r) = run(s)
+      f(a).run(r)
+    })
 }
 
 sealed trait Input
